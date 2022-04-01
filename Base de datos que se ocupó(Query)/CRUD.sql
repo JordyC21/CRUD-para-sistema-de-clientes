e8852a971f6d48983c465bbe76eff05cadf57fd2
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2022-03-31 22:24:34 -0600

    Update

---
+++ Base de datos que se ocupó(Query)/CRUD.sql
@@ -0,0 +1,13 @@
+create database Tienda
+
+use Tienda
+
+create table Cliente (
+idCliente int primary key identity (1,1) NOT NULL ,
+Nombre Varchar (50),
+Apellido Varchar (50),
+Edad int,
+Sexo varchar (20),
+)
+
+
